42db72f5c03007ba22c3219817d1c95a2062ff06
Mdzaidsiddique <45.mdzaid@gmail.com>
2022-11-04 15:10:28 +0530

    code fixed from my side

--- prime.js
+++ prime.js
@@ -1,3 +1,19 @@
-for(let i=string.length-1;i>=0){
-	new_str+=string[i];
+function checkPrime(number) {
+	if (numer <= 1) {
+		return false;
+	}
+	for (let i = 2; i <= number ** 0.5; i++){
+		if (number % i == 0) {
+			return false;
+		}
+		else {
+			return true
+		}
+	}
+}
+let answer = checkPrime(13);
+if (answer == true) {
+	console.log('Prime')
+} else {
+	console.log('Not a Prime')
 }
